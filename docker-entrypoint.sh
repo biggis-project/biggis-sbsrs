6f3dc512680b3d2c61aba8980fc794c9a567634a
Jochen Lutz <jlu@gmx.de>
2017-07-03 12:47:38 +0200

    clean up in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,7 @@
 
 SECRET=`cat /opt/sbsrs/sbsrs.secret`
 
+### clean up after unclean shutdown
+rm RUNNING_PID
+
 java -Dhttp.port=9100 -Dplay.crypto.secret="'$SECRET'" -Dsbsrs.kafkaServer="$KAFKA_SERVER" -jar /opt/sbsrs/sbsrs.jar
