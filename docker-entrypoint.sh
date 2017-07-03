aa98280b50d7bec06c20d8f593755dcc5148db69
Jochen Lutz <jlu@gmx.de>
2017-07-03 16:43:54 +0200

    Fixed test on RUNNING_PID

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 SECRET=`cat /opt/sbsrs/sbsrs.secret`
 
 ### clean up after unclean shutdown
-test -x RUNNING_PID && rm RUNNING_PID
+test -e RUNNING_PID && rm RUNNING_PID
 
 java -Dhttp.port=9100 -Dplay.crypto.secret="'$SECRET'" -Dsbsrs.kafkaServer="$KAFKA_SERVER" -jar /opt/sbsrs/sbsrs.jar
